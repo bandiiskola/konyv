0293440ef02863c348dcd5b1159c57dd1a579c8b
bandiiskola <nandor.bere@gmail.com>
2023-03-13 14:43:40 +0100

    félkész lapozás

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
+var oldalszam=1
 function btn() 
 {
     var szoveg = document.getElementById("textarea").value
     document.getElementById("bal_oldal").innerHTML = szoveg.substring(0, 324)
     document.getElementById("jobb_oldal").innerHTML = szoveg.substring(325,649) 
+}
+
+function lapoz_r()
+{
+    document.getElementById("bal_oldal").innerHTML ="" 
+    document.getElementById("jobb_oldal").innerHTML ="" 
+}
+
+function lapoz_l()
+{
+    var szoveg = document.getElementById("textarea").value
+    document.getElementById("bal_oldal").innerHTML ="" 
+    document.getElementById("jobb_oldal").innerHTML ="" 
+    
 }
